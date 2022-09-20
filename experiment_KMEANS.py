35e92f4c9453c5aac556e7a608617e7554114764
Niccolò Simonato <879993@stud.unive.it>
2022-09-20 12:16:40 +0200

    experiments_KMEANS.py now contains a user-friendly output

--- experiment_KMEANS.py
+++ experiment_KMEANS.py
@@ -25,14 +25,17 @@ ray.shutdown()
 ray.init()
 
 # Importing Data
+print(f"[{datetime.now()}]IMPORTING DATA...")
 test_data = pd.read_csv("Data/sessions_cleaned.csv", sep=",", skipinitialspace=True, skipfooter=3,
                         engine='python')
 
+print(f"[{datetime.now()}]REDUCING DIMENSIONALITY...")
 n_dims = pd.Series([i for i in range(8, 15)], index=[str(i) for i in range(8, 15)])
 
 pca_data = n_dims.apply(lambda n_dim: (reduce_dimensionality(data=test_data, n_final_features=n_dim), n_dim))
 
 # Settings
+print(f"[{datetime.now()}]GENERATING SETTINGS...")
 EXP_NUM = 3
 
 settings_KMEANS = {'n_init': 10,
@@ -50,71 +53,67 @@ settings_CBOD = {
     'epsilon': 0.005
 }
 
+print(f"[{datetime.now()}]GENERATING HYPERPARAMETERS CANDIDATES...")
 param = [{'n_clusters': i} for i in range(11, 17)]
 
 # %%time
-
+print(f"[{datetime.now()}]STARTING MATR...")
 timestamp1 = datetime.now()
-
 aux1 = pca_data.apply(lambda data: MATR(A=kmeans, D=data[0], hyperpar=param, settings=settings_KMEANS, verbose=1,
                                         path="Data/Results/Experiments/",
                                         name=f"[{EXP_NUM}]Experiment - PCA_{data[1]}_dim-KMeans"))
+timestamp1 = datetime.now() - timestamp1
+print(f"[{datetime.now()}]DONE! Time elapsed:\t{timestamp1}...")
+
+print(f"[{datetime.now()}]CALCULATING SILHOUETTE SCORES...")
 aux1.name = 'MATR'
 aux2 = pca_data.apply(lambda data: data[1])
 aux2.name = 'PCA_dim'
 aux3 = aux1.apply(lambda data: silhouette_score(X=data[list(set(data.columns) - {'cluster'})], labels=data['cluster']))
 aux3.name = 'silhouette'
 
-# result = pd.concat([aux1, aux2, aux3], axis=1, names=["MATR","PCA_dim","silhouette"])
-# print(f"aux1:\n{aux1.shape}")
-# aux1.info()
-# print(f"\naux2:\n{aux2.shape}")
-# aux2.info()
-# print(f"\naux3:\n{aux3.shape}")
-# aux3.info()
-
 result = pd.concat([aux2, aux3], axis=1, names=["PCA_dim", "silhouette"])
 
-timestamp1 = datetime.now() - timestamp1
-
-# %%time
-# Printing results
+print(f"[{datetime.now()}]PRINTING SILHOUETTE SCORES TO FILE...")
 timestamp2 = datetime.now()
 result["silhouette"].plot(kind="bar", xlabel="Number of dimensions after PCA", ylabel="Silhouette Score",
                           figsize=(35, 30)).get_figure().savefig(
     f'Data/Results/Experiments/PCA-KMeans_sil_score{EXP_NUM}.png')
 timestamp2 = datetime.now() - timestamp2
+print(f"[{datetime.now()}]DONE! Time elapsed:\t{timestamp2}...")
 
-# % % time
+print(f"[{datetime.now()}]PRINTING CLUSTERING PAIRPLOTS TO FILES...")
 # Printing the clusters
 timestamp3 = datetime.now()
-
-aux1.apply(lambda res: visualize_cluster(data=res,
-                                         i=EXP_NUM,
-                                         cluster_or_outliers='cluster',
-                                         additional=f"PCA_{len(res.columns) - 1}_dim-KMEANS_{res['cluster'].max() + 1}",
-                                         path="Data/Results/Experiments/"))
+aux1.apply(lambda x: visualize_cluster(data=x,
+                                       i=EXP_NUM,
+                                       cluster_or_outliers='cluster',
+                                       additional=f"PCA_{len(x.columns) - 1}_dim-KMEANS_{x['cluster'].max() + 1}",
+                                       path="Data/Results/Experiments/"))
 
 timestamp3 = datetime.now() - timestamp3
+print(f"[{datetime.now()}]DONE! Time elapsed:\t{timestamp3}...")
 
-# % % time
+print(f"[{datetime.now()}]OUTLIER DETECTION (with 8 dimensions)...")
+print(f"[{datetime.now()}]{'=' * 5} LDOF {'=' * 5}")
 # Outlier detection JUST 8 DIMENSIONS
 timestamp4 = datetime.now()
-
 res = aux1['8']
-
 det_ldof = top_n_LDOF(data=res, distance=euclidean_distance, n=settings_LDOF['n'], k=settings_LDOF['k'])
-
 det_ldof.info()
-
 timestamp4 = datetime.now() - timestamp4
+print(f"[{datetime.now()}]DONE! Time elapsed:\t{timestamp4}...")
+print(f"[{datetime.now()}]{'=' * 5}---{'=' * 5}")
 
 # %%time
+print(f"[{datetime.now()}]{'=' * 5} CBOD {'=' * 5}")
 timestamp5 = datetime.now()
 det_cbod = CBOD(data=res, k=res['cluster'].max() + 1, epsilon=settings_CBOD['epsilon'])
 
 det_cbod.info()
 timestamp5 = datetime.now() - timestamp5
+print(f"[{datetime.now()}]DONE! Time elapsed:\t{timestamp5}...")
+print(f"[{datetime.now()}]{'=' * 5}---{'=' * 5}")
 
 # %%time
 visualize_cluster(data=det_ldof[list(set(det_ldof.index) - {'cluster'} - {'LDOF'})],
