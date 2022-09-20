df49302994e2918b5edc552364e6d950504c04b3
Niccolò Simonato <879993@stud.unive.it>
2022-09-20 14:18:58 +0200

    CBOD.py bugfix

--- OutliersDetection/CBOD.py
+++ OutliersDetection/CBOD.py
@@ -50,5 +50,5 @@ def CBOD(data: DataFrame, k: int, epsilon: float) -> ([], []):
     while b < k and (sum(map(lambda x: x.shape[0], clusters[0:b])) / data.shape[0]) > epsilon:
         b += 1
 
-    data.loc[:, "outlier"] = data['cluster'].apply(lambda x: True if x <= b else False)
+    data["outlier"] = data['cluster'].apply(lambda x: True if x <= b else False)
     return clusters_repr
