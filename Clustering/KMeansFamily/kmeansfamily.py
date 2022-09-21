23bb0592596da065814e5fdff8e0ac2e12f4108e
nsimonato <879993@stud.unive.it>
2022-09-21 18:19:00 +0200

    KMeans adjustments

--- Clustering/KMeansFamily/kmeansfamily.py
+++ Clustering/KMeansFamily/kmeansfamily.py
@@ -10,37 +10,37 @@
 # 3.Update step:
 #   a.Recalculate means (centroids) for observations assigned to each cluster.
 # 4.The algorithm has converged when the assignments no longer change.
-from modin.pandas import DataFrame
-# from pandas import DataFrame
+import modin.pandas as pd
+# from pandas import pd.DataFrame
 from sklearn.cluster import KMeans
 from sklearn_extra.cluster import KMedoids
 
 
-def kmeans(data: DataFrame, hyperpar: dict, settings: dict):
+def kmeans(data: pd.DataFrame, hyperpar: dict, settings: dict):
     """ 
     This functions assings a cluster number to each instance of data.
     :param settings: The dictionary of the algorithm's settings.
     :param hyperpar: The dictionary of the hyperparameters' values.
-    :param data: This Dataframe contains the data to be clustered.
-    :return The initial DataFrame, but with a new column, with a progressive id for each cluster.
+    :param data: This pd.DataFrame contains the data to be clustered.
+    :return The initial pd.DataFrame, but with a new column, with a progressive id for each cluster.
     """
     fit = KMeans(n_clusters=hyperpar['n_clusters'], init='k-means++', n_init=settings['n_init'],
                  max_iter=settings['max_iter'],
                  tol=0.0001, verbose=settings['verbose'], random_state=None, copy_x=True,
                  algorithm=settings['algorithm'])
-    data.loc['cluster'] = fit.fit_predict(data)
+    data.loc['cluster'] = pd.Series(fit.fit_predict(data))
     return data
 
 
-def kmedoids(data: DataFrame, hyperpar: dict, settings: dict):
+def kmedoids(data: pd.DataFrame, hyperpar: dict, settings: dict):
     """
     This functions assings a cluster number to each instance of data.
     :param hyperpar: The dictionary of the hyperparameters' values.
-    :param data: This Dataframe contains the data to be clustered.
+    :param data: This pd.DataFrame contains the data to be clustered.
     :param settings: This dictionary contains the settings for the optimization of the algorithm's performance.
-    :return DataFrame
+    :return pd.DataFrame
     """
     fit = KMedoids(n_clusters=hyperpar['n_clusters'], metric=hyperpar['metric'], init=settings['init'],
                    max_iter=settings['max_iter'], random_state=settings['random_state'])
-    data.loc['cluster'] = fit.fit_predict(data)
+    data.loc['cluster'] = pd.Series(fit.fit_predict(data))
     return data
