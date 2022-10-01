073ff0309cfab017a9f7a94ca41f0bd83ce55c7f
nsimonato <879993@stud.unive.it>
2022-10-01 21:31:19 +0200

    DBSCAN_stats.py debug

--- DBSCAN_stats.py
+++ DBSCAN_stats.py
@@ -5,7 +5,7 @@ from scipy.spatial.distance import euclidean
 from Utils.algebric_op import similarity_matrix
 
 print("EPSILON TEST - [DBSCAN]:\nImporting data...")
-test_data = pd.read_csv("../Data/sessions_cleaned.csv", sep=",", skipinitialspace=True, skipfooter=3,
+test_data = pd.read_csv("Data/sessions_cleaned.csv", sep=",", skipinitialspace=True, skipfooter=3,
                         engine='python')  # Importing the sample data
 
 print("similarity_matrix()")
@@ -18,4 +18,4 @@ print("pd.Series.drop_duplicates()")
 distances = pd.Series(distances).drop_duplicates()
 print("plot(...)")
 distances.plot(kind="bar", xlabel="Distance values", ylabel="Frequence",
-               figsize=(35, 30)).get_figure().savefig(f'../Elbow_euclidean_similarities.png')
+               figsize=(35, 30)).get_figure().savefig(f'Elbow_euclidean_similarities.png')
