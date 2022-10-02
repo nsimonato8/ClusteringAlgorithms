23a7acfec83cf74f6216d56ef9d9f7ee68456d1b
nsimonato <879993@stud.unive.it>
2022-10-02 11:53:59 +0200

    experiment_KMEANS.py parameter adjustments; DBSCAN_stats.py parameter adjustments

--- DBSCAN_stats.py
+++ DBSCAN_stats.py
@@ -22,14 +22,7 @@ print("EPSILON TEST - [DBSCAN]:\nImporting data...")
 test_data = pd.read_csv(f"Data/{FILENAME}sessions_cleaned.csv", sep=",", skipinitialspace=True,
                         skipfooter=3)  # Importing the sample data
 n_bins = 20
-x_lim = (2 * (10 ** 6), 2.5 * (10 ** 6))
-
-# print("Calculating distances [TEST_DATA]")
-# distances = pd.Series(np.matrix.flatten(similarity_matrix(test_data, euclidean).to_numpy())).drop_duplicates()
-# print("plot(...)")
-# distances.plot(kind="hist", xlabel="Distance values", ylabel="Frequence",
-#                figsize=(35, 30), bins=n_bins, xlim=x_lim).get_figure().savefig(
-#     f'{FILENAME}Elbow_euclidean_similarities.png')
+x_lim = (2.3 * (10 ** 6), 2.4 * (10 ** 6))
 
 print("Calculating distances [8_DIM_DATA]")
 distances = pd.Series(np.matrix.flatten(similarity_matrix(reduce_dimensionality(data=test_data, n_final_features=8),
